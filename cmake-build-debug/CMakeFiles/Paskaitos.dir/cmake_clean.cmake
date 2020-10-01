fcd130a0632cbb3b0d9702f5c461b4c34fa934a7
Your NameMindaugas <lupeikismindaugas@gmail.com>
2020-10-01 15:29:30 +0300

    pirmas

---
+++ cmake-build-debug/CMakeFiles/Paskaitos.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Paskaitos.dir/main.cpp.obj"
+  "Paskaitos.exe"
+  "Paskaitos.exe.manifest"
+  "Paskaitos.pdb"
+  "libPaskaitos.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Paskaitos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream> // visada reikia prideti, kai naudojam string etc...
+#include <vector> // kai naudojas masyvus (panasiai kaip listai, kuriu c++ nera)
+#include <cmath> // matematiniams veiksmams atlikti
+
+// using namespace std; // sita naudojam jei nenorim naudoti std:: prie cout ar endl etc. / nera gera praktika
+// pries main() apsirasom deklaracijas
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    // cout - kaip print()
+
+    return 0;
+    // The return value is the exit code of your program, the shell (or any other application that ran it) can read and use it.
+    //The 0 exit code is a widely accepted convention for 'OK the program execution was successfull'.
+}
